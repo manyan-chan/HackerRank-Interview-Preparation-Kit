5d8ccc7055083aff7540d47610bbb879b4702c1e
Manyan Chan <ss121317@connect.hku.hk>
2021-12-03 11:12:57 +0800

    Arrays

--- hackerank.py
+++ hackerank.py
@@ -39,3 +39,56 @@ def jumpingOnClouds(c):
 
 def repeatedString(s, n):
     return s.count("a") * (n // len(s)) + s[: n % len(s)].count("a")
+
+
+# Arrays
+def hourglassSum(arr):
+    sums = []
+    for i in range(len(arr) - 2):
+        for j in range(len(arr[i]) - 2):
+            sums.append(
+                sum(arr[i][j : j + 3]) + arr[i + 1][j + 1] + sum(arr[i + 2][j : j + 3])
+            )
+    return max(sums)
+
+
+def rotLeft(a, d):
+    return a[d:] + a[:d]
+
+
+def minimumBribes(q):
+    bribes = 0
+    for i in range(len(q)):
+        if q[i] - (i + 1) > 2:
+            print("Too chaotic")
+            return
+        for j in range(max(0, q[i] - 2), i):
+            if q[j] > q[i]:
+                bribes += 1
+    print(bribes)
+
+
+def minimumSwaps(arr):
+    swaps = 0
+    for i in range(len(arr)):
+        while arr[i] != i + 1:
+            temp = arr[i]
+            arr[i] = arr[temp - 1]
+            arr[temp - 1] = temp
+            swaps += 1
+    return swaps
+
+
+def arrayManipulation(n, queries):
+    arr = [0] * (n + 1)
+    for query in queries:
+        arr[query[0] - 1] += query[2]
+        if query[1] <= n:
+            arr[query[1]] -= query[2]
+    max_val = 0
+    val = 0
+    for i in range(len(arr)):
+        val += arr[i]
+        if val > max_val:
+            max_val = val
+    return max_val
